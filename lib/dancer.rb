ae881a951e4876ded5681cba810e617d3d6aca47
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-16 08:08:49 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,6 +3,11 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
